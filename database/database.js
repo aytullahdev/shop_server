7b76d6605c3062456b9d8c94c809a0abead47b64
aytullahdev <ayt.ullah.dev@gmail.com>
2023-04-09 06:23:11 +0600

    online database added

--- database/database.js
+++ database/database.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
-var con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "password",
-  database: "shop",
-});
 // var con = mysql.createConnection({
-//   host: "sql.freedb.tech",
-//   user: "freedb_aytdev",
-//   password: "4QCaAqKxc%Ydv&K",
-//   database: "freedb_myshop",
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "password",
+//   database: "shop",
 // });
+var con = mysql.createConnection({
+  host: "sql12.freemysqlhosting.net",
+  user: "sql12611997",
+  password: "byxZNvILpm",
+  database: "sql12611997",
+});
 // con.connect(function (err) {
 //   if (err) console.log(err);
 //   console.log("Connected!");
@@ -29,7 +29,28 @@ try {
     // var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
     //var sql ="CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
     // var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    // var sql = `SELECT * FROM category`;
+    // var sql = `CREATE TABLE category (
+    //   id int NOT NULL AUTO_INCREMENT,
+    //   gender char(5) NOT NULL,
+    //   name varchar(45) NOT NULL,
+    //   PRIMARY KEY (id),
+    //   UNIQUE KEY id_UNIQUE (id)
+    // )`;
+    // var sql = `CREATE TABLE product (
+    //   id int NOT NULL AUTO_INCREMENT,
+    //   title varchar(100) NOT NULL,
+    //   description text NOT NULL,
+    //   price int NOT NULL DEFAULT '0',
+    //   category_id varchar(100) NOT NULL,
+    //   stock varchar(45) DEFAULT NULL,
+    //   img varchar(200) DEFAULT NULL,
+    //   img2 varchar(200) DEFAULT NULL,
+    //   img3 varchar(200) DEFAULT NULL,
+    //   note varchar(200) DEFAULT NULL,
+    //   sku varchar(45) DEFAULT NULL,
+    //   PRIMARY KEY (id),
+    //   UNIQUE KEY id_UNIQUE (id)
+    // )`;
     // con.query(sql, function (err, result, fields) {
     //   if (err) {
     //     console.log("Error");
